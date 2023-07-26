205b2b671c46f67df218b9ee947a8c75ca5002f7
Jerome Wang immjs <informathemusic@gmail.com>
2023-07-26 21:02:27 +0800

    Allow for UID change

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ import { startConsume } from './queue.js';
 
 await StartHTTP();
 await startConsume();
+
+if (process.env.UID && !Number.isNaN(+process.env.UID) && process.setuid) process.setuid(+process.env.UID);
